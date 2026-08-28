2f6db7dec842a8418e1eaa1bf5ad7654ba938912
Joe Invisible <invisikillerjoe@gmail.com>
2026-08-28 10:25:37 +0800

    updated dcmotor test

--- Tests/Src/TestMain.c
+++ Tests/Src/TestMain.c
@@ -6,7 +6,20 @@
  */
 
 #include "DCMotorTestBasic.h"
+#include "OLEDTest.h"
+#include "oled.h"
+
+#include "userbutton.h"
 
 void InvokeTest() {
+	OLED_Init();
+	OLED_Clear();
+	OLED_ShowString5x7(0, 0, (const char*)"BTT: Press SW1 to start test.");
+	OLED_Refresh_Gram();
+	SW1_WhileNotPressed();
+
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET);
+
 	DCMotorTestRun();
+	SW1_WhileNotPressed();
 }
