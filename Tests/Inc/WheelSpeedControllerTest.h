5a62153f5d873126ac3932c8799d48a450ab3a87
Joe Invisible <invisikillerjoe@gmail.com>
2026-08-29 20:37:16 +0800

    tests: updated tests

---
+++ Tests/Inc/WheelSpeedControllerTest.h
@@ -0,0 +1,16 @@
+/*
+ * WheelSpeedControllerTest.h
+ *
+ *  Created on: 2026年8月29日
+ *      Author: Joe
+ */
+
+#ifndef INC_WHEELSPEEDCONTROLLERTEST_H_
+#define INC_WHEELSPEEDCONTROLLERTEST_H_
+
+#include "WheelSpeedController.h"
+#include "rwdriver.h"
+
+void WheelSpeedControllerTestRun();
+
+#endif /* INC_WHEELSPEEDCONTROLLERTEST_H_ */
--- Tests/Src/TestMain.c
+++ Tests/Src/TestMain.c
@@ -10,21 +10,19 @@
 #include "ServoTestBasic.h"
 #include "ICM20948Test.h"
 #include "OLEDTest.h"
+#include "WheelSpeedControllerTest.h"
 #include "oled.h"
 #include "led3.h"
 
 void InvokeTest() {
 	OLED_Init();
 	OLED_Clear();
-//	OLED_ShowString5x7(0, 0, (const char*)"BTT: Press SW1 to start test.");
-//	OLED_Refresh_Gram();
-//	SW1_WhileNotPressed();
 
 	LED_On();
 
 	HAL_Delay(200); // Because Init is too fast
 
-	DCMotorTestRun();
+	WheelSpeedControllerTestRun();
 
 	SW1_WhileNotPressed();
 
