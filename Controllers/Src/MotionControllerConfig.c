1305a6d95805661679329d29d9dcf3f47af22d98
Joe Invisible <invisikillerjoe@gmail.com>
2026-09-01 13:24:44 +0800

    updated wheel diameter measurement

--- Controllers/Src/MotionControllerConfig.c
+++ Controllers/Src/MotionControllerConfig.c
@@ -8,11 +8,13 @@
 #include "MotionControllerConfig.h"
 
 #define MG512P30_QUAD_RESOLUTION		1560U
-#define WHEEL_DIAMETER_MM			65.0f
 #define WHEELBASE_MM					150.0f
 #define FRONT_TRACK_WIDTH_MM			165.0f
 #define REAR_TRACK_WIDTH_MM			164.0f
-// We need to measure the effective metrics of the wheels
+/**
+ * Effective wheel diameter under load.
+ */
+#define WHEEL_DIAMETER_MM			65.5f
 
 const RobotKinematics kinematics = {
 		.rearEncoderCountsPerRev = MG512P30_QUAD_RESOLUTION,
