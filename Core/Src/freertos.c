052d44cfba6f946b3c5666e50c0482657914f24d
Joe Invisible <invisikillerjoe@gmail.com>
2026-09-06 14:11:29 +0800

    minor: moved inclusion into user code area

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -22,11 +22,10 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
-#include "AppTasks.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "AppTasks.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
