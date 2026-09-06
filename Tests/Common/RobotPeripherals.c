84886aa212c5e90b8dca1273bd50d63c884e0322
Joe Invisible <invisikillerjoe@gmail.com>
2026-09-06 17:32:24 +0800

    arch: centralised peripheral definition file

---
+++ Tests/Common/RobotPeripherals.c
@@ -0,0 +1,10 @@
+/*
+ * RobotPeripherals.c
+ *
+ *  Created on: 2026年9月6日
+ *      Author: Joe
+ */
+
+#include "RobotPeripherals.h"
+
+HCSR04_HandleTypeDef hcsr04;
---
+++ Tests/Common/RobotPeripherals.h
@@ -0,0 +1,17 @@
+/*
+ * RobotPeripherals.h
+ *
+ * Global definitions of the physical peripheral instances on the robot.
+ *
+ *  Created on: 2026年9月6日
+ *      Author: Joe
+ */
+
+#ifndef INC_ROBOTPERIPHERALS_H_
+#define INC_ROBOTPERIPHERALS_H_
+
+#include "hcsr04.h"
+
+extern HCSR04_HandleTypeDef hcsr04;
+
+#endif /* INC_ROBOTPERIPHERALS_H_ */
